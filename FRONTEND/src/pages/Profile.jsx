b0470276766f406fead59ba6f11ab4b689483522
Ayansh-Jain <ayanshjain2006@gmail.com>
2025-05-16 02:45:09 +0530

    77

--- FRONTEND/src/pages/Profile.jsx
+++ FRONTEND/src/pages/Profile.jsx
@@ -335,7 +335,7 @@ const Profile = () => {
                 <div key={`${post._id}-${i}`} className="post-minimized" onClick={() => openPostModal(post)}>
                   {post.img ? (
                     isVideo(post.img) ? (
-                      <video src={post.img} preload="metadata" className="post-thumbnail" muted playsInline/>
+                      <video src={post.img} preload="metadata" className="post-thumbnail"/>
                     ) : (
                       <img src={post.img} alt="Post thumbnail" className="post-thumbnail" />
                     )
